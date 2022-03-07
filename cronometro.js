6734ae150166d543b064513da53b351c526cb15d
Matheus Henrique <80555392+matheusman@users.noreply.github.com>
2022-03-07 13:45:09 -0300

    Update cronometro.js

--- cronometro.js
+++ cronometro.js
@@ -1,84 +1,91 @@
-let start = document.getElementById('start')
-let restart = document.getElementById('restart')
-let container = document.getElementById('funcionalidade')
-let text = document.getElementById('cronometro')
-let mm = -1
-let sec = 0
-let min = 0
+let thousandth = 0
+let seconds = 0
+let minutes = 0
+
+let voltarMinutes = 0
+let voltarSeconds = 0
+let voltarthousandth = 0
+
 let cont = 0
-let result1 = 0
-var interval
-var timeOut
+var myinterval;
 
-let cronometroText = () => {
-    mm++
-    if (mm === 100) {
-        mm = 0
-        sec++
+const myClearInterval = () => clearInterval(myinterval)
+
+const interval = () => myinterval = setInterval(() => {
+
+    thousandth++
+    if (thousandth === 60) {
+        seconds++
+        thousandth = 0   
     }
-    if (sec === 60) {
-        sec = 0
-        min++
+    else if (seconds === 60) {
+        minutes++
+        seconds = 0
+    } else if (minutes === 24) {
+        minutes = 0
     }
-    let milesimos = mm < 10 ? "0" + mm : mm
-    let segundos = sec < 10 ? "0" + sec : sec
-    let minutos = min < 10 ? "0" + min : min
-    return text.innerHTML = `<h1>${minutos}:${segundos}</h1><h3>:${milesimos}</h3>`
+
+    voltarthousandth++
+    if (voltarthousandth === 60) {
+        voltarSeconds++
+        voltarthousandth = 0
+    } 
+    else if (voltarSeconds === 60) {
+        voltarMinutes++
+        voltarSeconds = 0
+    } else if (voltarMinutes === 60) {
+        voltarMinutes = 0
+    }
+
+    const divs = `<h3>${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${thousandth.toString().padStart(2, '0')}</h3>`
+    const divsVolt =  `<h3>${voltarMinutes.toString().padStart(2, '0')}:${voltarSeconds.toString().padStart(2, '0')}:${voltarthousandth.toString().padStart(2, '0')}</h3>`
+    document.getElementById('timer').innerHTML = divs
+    document.getElementById('number').innerHTML = cont + 1
+    document.getElementById('hora').innerHTML = divsVolt
+    document.getElementById('number-fixed').innerHTML = divs
+}, 10)
+
+function startCronometro() {
+    myClearInterval()
+    interval()
+    document.getElementById('start').style.display = 'none'
+    document.getElementById('restart').style.display = 'none'
+    document.getElementById('mark').style.display = 'block'
+    document.getElementById('stop').style.display = 'block'
 }
 
-const returnButton = (text, callback) => {
-    const button = document.createElement('button')
-    button.innerHTML = text
-    callback(button)
-    container.insertAdjacentElement('afterbegin', button)
-    return button 
+function restartCronometro () {
+    myClearInterval()
+    document.getElementById('timer').innerHTML = `<h3>00:00:00</h3>`
+    document.getElementById('voltar').innerHTML = ''
+    thousandth = 0
+    seconds = 0
+    minutes = 0
+    voltarMinutes = 0
+    voltarSeconds = 0
+    voltarthousandth = 0
 }
 
-let contagem = () => {
-    interval = setInterval(() => cronometroText(), 10)
-    container.removeChild(restart)
-    container.removeChild(start)
-    returnButton("Stop", (button) => {
-        button.addEventListener('click', clear)
-        button.addEventListener('mouseover', () => {button.style = `background-color: #e79427; color: white;`})
-        button.addEventListener('mouseout', () => {button.style = `background-color: orange;color: white;`})
-    })
-    returnButton("Voltar", (button) => {
-        button.addEventListener('click', voltarTimer)
-        button.style = `background-color: #000000; color: white;`
-        button.addEventListener('mouseover', () => {button.style = `background-color: #444444; color: white;`})
-        button.addEventListener('mouseout', () => {button.style = `background-color: #000000; color: white;`})
-    })
-}
- 
-let clear = () => {
-    const botao = document.querySelectorAll('#funcionalidade button')
-    container.removeChild(botao[0])
-    container.removeChild(botao[1])
-    container.insertAdjacentElement('afterbegin', start)
-    container.insertAdjacentElement('afterbegin', restart)
-    clearInterval(interval)
+function stopCronometro () {
+    myClearInterval()
+    document.getElementById('start').style.display = 'block'
+    document.getElementById('restart').style.display = 'block'
+    document.getElementById('mark').style.display = 'none'
+    document.getElementById('stop').style.display = 'none'
 }
 
-let voltarTimer = () => {
+function voltarCronometro () {
     cont++
-    let milesimos = mm < 10 ? "0" + mm : mm
-    let segundos = sec < 10 ? "0" + sec : sec
-    let minutos = min < 10 ? "0" + min : min
-    let voltar = document.getElementById('voltar')
-    voltar.innerHTML = `<h3>Volta</h3><h3>Hora</h3><h3>Duração total</h3>`
-    voltar.innerHTML += `<p>${cont}</p>`
-    voltar.innerHTML += `<p>${minutos}:${segundos}:${milesimos}</p>`
-    voltar.innerHTML += `<p>${minutos}:${segundos}:${milesimos}</p>`
+    const voltar = document.getElementById('voltar')
+    const timer = document.getElementById('timer')
+    const hora = document.getElementById('hora')
+    voltar.innerHTML += `<div class="voltarTimerId"><div class="number">${cont}</div><div>${hora.innerHTML}</div><div>${timer.innerHTML}</div></div>`
+    voltarMinutes = 0
+    voltarSeconds = 0
+    voltarthousandth = 0
 }
 
-let restartTimer = () => {
-    mm = sec = min = 0
-    cont = 0
-    clearInterval(interval)
-    text.innerHTML = `<h1>00:00</h1><h3>:00</h3>`
-}
-
-cronometroText(mm)
-start.addEventListener('click', contagem)
-restart.addEventListener('click', restartTimer)
+document.getElementById('start').addEventListener('click', startCronometro)
+document.getElementById('restart').addEventListener('click', restartCronometro)
+document.getElementById('stop').addEventListener('click', stopCronometro)
+document.getElementById('mark').addEventListener('click', voltarCronometro)
